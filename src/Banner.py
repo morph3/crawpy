8feb8846934091a8b0470a62c357fdcfae86f3d0
morph3 <melihkaanyildiz@gmail.com>
2020-11-05 02:56:04 +0300

    Update Banner.py

--- src/Banner.py
+++ src/Banner.py
@@ -39,5 +39,5 @@ class Banner:
         """)
 
     def greet(self):
-        print ("\n                                              Crawpy v1.1",end="")
+        print ("\n                                              Crawpy v1.0",end="")
         print (self.banners[random.randint(0, len(self.banners) - 1)])
